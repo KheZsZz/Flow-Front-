7d367b940696abcf1748c01bab41cb4ded664757
kevin Oliveira <kevinklgvg@gmail.com>
2026-06-15 17:24:58 -0300

    create service status

---
+++ src/services/status.ts
@@ -0,0 +1,47 @@
+import { StatusTypes } from "@/schemas/statusSchema";
+import { api } from "./api";
+
+export const getStatus = async () => {
+  try {
+    const response = await api.get("/status");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createStatus = async (data: StatusTypes) => {
+  try {
+    const response = await api.post("/status", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateStatus = async (id: string, data: StatusTypes) => {
+  try {
+    const response = await api.put(`/status/${id}`, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteStatus = async (id: string) => {
+  try {
+    const response = await api.delete(`/status/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findStatus = async (id: string) => {
+  try {
+    const response = await api.get(`/status/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
